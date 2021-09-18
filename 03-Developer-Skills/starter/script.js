01a551e3f45936924e0890fe593893e28da8df4b
Ellinor Carlberg <ellinor.carlberg@medieinstitutet.se>
2021-09-18 16:50:33 +0200

    done with section developer skill and editor setup

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -142,15 +142,57 @@ array8.push(' = ');
 
 console.log(array8.length);
   */
-
+/* 
 const measureKelvin = function () {
   const measurement = {
     type: 'temp',
     unit: 'celcius',
-    value: prompt('Degrees celcius:'),
+    value: Number(prompt('Degrees celcius:')),
   };
-  console.log(typeof measurement.value);
+
   const kelvin = measurement.value + 273;
+
   return kelvin;
 };
+console.log('hej');
 console.log(measureKelvin());
+ */
+
+///////////////////////////////////////
+// Coding Challenge #1
+
+/*
+Given an array of forecasted maximum temperatures, the thermometer displays a string with these temperatures.
+
+Example: [17, 21, 23] will print "... 17ºC in 1 days ... 21ºC in 2 days ... 23ºC in 3 days ..."
+
+Create a function 'printForecast' which takes in an array 'arr' and logs a string like the above to the console.
+
+Use the problem-solving framework: Understand the problem and break it up into sub-problems!
+
+TEST DATA 1: [17, 21, 23]
+TEST DATA 2: [12, 5, -5, 0, 4]
+*/
+
+// 1) Understanding the problem
+// skapa en funktion, ska kunna ta en en array med siffror.
+// för varje siffra i array ska en sträng printas ut t.ex så här: ... 17ºC in 1 days
+// array kan vara hur lång/kort som helst
+
+// 2) Breaking up into sub-problems
+// skapa en function
+// tar in en array med nummer
+//retunerar en sträng
+// varje siffra i arrayen ska retunera en del i strängen
+
+function printForecast(forecastArray) {
+  const stringArray = [];
+  for (let i = 0; i < forecastArray.length; i++) {
+    stringArray.push(`... ${forecastArray[i]}ºC in ${i + 1} days`);
+  }
+
+  const forecastString = stringArray.join(' ');
+  return forecastString + ' ...';
+}
+
+console.log(printForecast([12, 5, -5, 0, 4]));
