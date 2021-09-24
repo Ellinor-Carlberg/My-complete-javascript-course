e983aede65a7b91fa3576e7c280a187851898082
Ellinor Carlberg <ellinor.carlberg@medieinstitutet.se>
2021-09-24 21:52:42 +0200

    06-modal done

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,29 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const closeModalbtn = document.querySelector('.close-modal');
+const openModalsbtn = document.querySelectorAll('.show-modal');
+console.log('openModal', openModalsbtn);
+
+for (let i = 0; i < openModalsbtn.length; i++) {
+  openModalsbtn[i].addEventListener('click', function () {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+  });
+}
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+closeModalbtn.addEventListener('click', closeModal);
+
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
